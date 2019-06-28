6f6a129c24dc6dddabd1eb8b23721758aec8ece3
gediaogege <1181639720@qq.com>
2019-06-28 17:34:53 +0800

    添加项目学习地址

--- admin/src/main/java/com/example/mall/AdminApplication.java
+++ admin/src/main/java/com/example/mall/AdminApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 项目学习地址https://github.com/macrozheng/mall-admin-web
+ */
 @SpringBootApplication
 @MapperScan("com.example.mall.mapper")
 public class AdminApplication {
